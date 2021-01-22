f2baf8f0169cb8500e7f1abbf9338dde8c00b3a2
imoren2x <imoren2x@users.sourceforge.net>
2021-01-22 23:46:00 +0100

    Addition of WindowsScripts folder.
    Signed-off-by: imoren2x <imoren2x@users.sourceforge.net>

---
+++ WindowsScripts/gitsm.bat
@@ -0,0 +1,8 @@
+git submodule update --checkout --recursive
+
+@echo off
+
+REM $> git submodule status
+REM $> git submodule sync [<path>] (NOOOOOO)
+REM $> git submodule update --checkout --remote --recursive [<path>] (Sí)
+REM $> git submodule foreach "gitk HEAD &"
