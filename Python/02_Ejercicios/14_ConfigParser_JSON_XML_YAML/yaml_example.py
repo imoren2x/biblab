c7fa7ee568655eb6d846d1d0be00ed361b5f9144
imoren2x <imoren2x@users.sourceforge.net>
2022-11-10 10:45:15 +0100

    Python example of YAML module corrected and updated to Python 3.
    Signed-off-by: imoren2x <imoren2x@users.sourceforge.net>

--- Python/02_Ejercicios/14_ConfigParser_JSON_XML_YAML/yaml_example.py
+++ Python/02_Ejercicios/14_ConfigParser_JSON_XML_YAML/yaml_example.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 
 """
+Python 3.0
 http://pyyaml.org/wiki/PyYAMLDocumentation
 """
 
 import os
 import yaml
+
+from yaml.loader import SafeLoader
 
 FILENAME = ".%s%s" % (os.sep, "yaml_input.yml")
 
@@ -14,8 +17,26 @@ FILENAME = ".%s%s" % (os.sep, "yaml_input.yml")
 def main():
     """
     """
-    yamlDict = yaml.load(file(FILENAME))
-    print("YAML: %s" % yamlDict)
+    # Read the YAML file
+    with open(FILENAME, 'r') as file_d:
+        yaml_d = yaml.load(file_d, Loader=SafeLoader)
+
+    print("YAML: %s" % yaml_d)
+
+    # Write the output file
+    #  sort_keys True by default
+    #  default_flow_style True by default
+    with open('yaml_output.yml', 'w') as output_d:
+        data = yaml.dump(
+            yaml_d,
+            output_d,
+            indent=4,
+            sort_keys=False,
+            default_flow_style=False
+        )
+
+    print("Output data: %s" % data)
+
     return
 
 
@@ -23,3 +44,10 @@ if __name__ == "__main__":
     """
     """
     main()
+
+"""
+References:
+* https://pynative.com/python-yaml/
+* https://pyyaml.org/wiki/PyYAMLDocumentation
+* https://realpython.com/python-yaml/
+"""
